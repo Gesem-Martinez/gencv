49e7719740b0711bc7643311e9a8796b68f9e151
Gesem-Martinez <109618325+Gesem-Martinez@users.noreply.github.com>
2024-06-19 10:57:46 -0600

    Refactor FormContainer component. Add buttons to clear inputs and add elements to education and experience forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,99 @@
-import { useState } from 'react';
+import { useState } from "react";
 import "./App.css";
 import Header from "./Header";
-import InputElem from "./InputElem";
-import Form from "./Form";
-import generalIcon from "./assets/account.svg";
-import educationIcon from "./assets/school.svg";
-import experienceIcon from "./assets/professional.svg";
+import FormContainer from "./FormContainer";
+
+function generalInfoHelper(e, state, setter) {
+  if (e.target.id === "First Name") {
+    setter({
+      ...state,
+      firstName: e.target.value,
+    });
+  } else if (e.target.id === "Last Name") {
+    setter({
+      ...state,
+      lastName: e.target.value,
+    });
+  } else if (e.target.id === "Email") {
+    setter({
+      ...state,
+      email: e.target.value,
+    });
+  } else if (e.target.id === "Phone Number") {
+    setter({
+      ...state,
+      phoneNumber: e.target.value,
+    });
+  } else if (e.target.id === "Location") {
+    setter({
+      ...state,
+      location: e.target.value,
+    });
+  } else if (e.target.id === "Summary") {
+    setter({
+      ...state,
+      summary: e.target.value,
+    });
+  }
+}
+
 
 function App() {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [location, setLocation] = useState('');
-  const [summary, setSummary] = useState('');
+  const [generalInfoStates, setGeneralInfoStates] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+    location: "",
+    summary: "",
+  });
+  
+  const [educationInfoStatesArr, setEducationInfoStatesArr] = useState([]);
 
-  const largeInput = 300;
-  const smallInput = 120;
-
-  const doubleSmInput = {
-    display: "flex",
-    justifyContent: "space-between",
-  };
+  function handleGeneralInfoSetter(e) {
+    generalInfoHelper(e, generalInfoStates, setGeneralInfoStates);
+    return;
+  }
 
   return (
     <>
       <Header />
       <div className="main-container">
-        <div className="forms-container">
-          <Form label="General Information" icon={generalIcon}>
-            <InputElem label="First Name" inputWidth={largeInput} value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
-            <InputElem label="Last Name" inputWidth={largeInput}  value={lastName} onChange={(e) => setLastName(e.target.value)}/>
-            <InputElem type='email' label="Email" inputWidth={largeInput} value={email} onChange={(e) => setEmail(e.target.value)}/>
-            <InputElem type='tel' label="Phone Number" inputWidth={largeInput}  value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)}/>
-            <InputElem label="Location" inputWidth={largeInput}  value={location} onChange={(e) => setLocation(e.target.value)}/>
-            <InputElem type='textarea' label="Summary" inputWidth={largeInput} value={summary} onChange={(e) => setSummary(e.target.value)}/>
-          </Form>
-
-          <Form label="Education" icon={educationIcon}>
-            <InputElem label="School" inputWidth={largeInput} />
-            <InputElem label="Field of Study" inputWidth={largeInput} />
-            <div style={doubleSmInput}>
-              <InputElem label="Start Date" inputWidth={smallInput} />
-              <InputElem label="End Date" inputWidth={smallInput} />
-            </div>
-          </Form>
-
-          <Form label="Experience" icon={experienceIcon}>
-            <InputElem label="Prueba" inputWidth={largeInput} />
-            <InputElem label="Field of Study" inputWidth={largeInput} />
-            <div style={doubleSmInput}>
-              <InputElem label="Start Date" inputWidth={smallInput} />
-              <InputElem label="End Date" inputWidth={smallInput} />
-            </div>
-          </Form>
-        </div>
+        <FormContainer
+          generalInfoStates={generalInfoStates}
+          setGeneralInfoStates={handleGeneralInfoSetter}
+          educationInfoArr={educationInfoStatesArr}
+          setEducationInfoArr={educationInfoStatesArr}
+        />
 
         <div className="preview-container">
-          <p className="preview-full-name">{firstName}<br/>{lastName}</p>
+          <p className="preview-full-name">
+            {generalInfoStates.firstName}
+            <br />
+            {generalInfoStates.lastName}
+          </p>
           <div className="preview-sidebar">
             <div className="preview-summary preview-sidebar-info">
-              <p className='preview-sidebar-header'>Summary</p>
-              <p className='preview-sidebar-body'>{summary}</p>
+              <p className="preview-sidebar-header">Summary</p>
+              <p className="preview-sidebar-body">
+                {generalInfoStates.summary}
+              </p>
             </div>
             <div className="preview-location preview-sidebar-info">
-              <p className='preview-sidebar-header'>Location</p>
-              <p className='preview-sidebar-body'>{location}</p>
+              <p className="preview-sidebar-header">Location</p>
+              <p className="preview-sidebar-body">
+                {generalInfoStates.location}
+              </p>
             </div>
             <div className="preview-email preview-sidebar-info">
-              <p className='preview-sidebar-header'>Email</p>
-              <p className='preview-sidebar-body'>{email}</p>
+              <p className="preview-sidebar-header">Email</p>
+              <p className="preview-sidebar-body">{generalInfoStates.email}</p>
             </div>
             <div className="preview-phone preview-sidebar-info">
-              <p className='preview-sidebar-header'>Phone Number</p>
-              <p className='preview-sidebar-body'>{phoneNumber}</p>
+              <p className="preview-sidebar-header">Phone Number</p>
+              <p className="preview-sidebar-body">
+                {generalInfoStates.phoneNumber}
+              </p>
             </div>
           </div>
         </div>
--- src/FormContainer.jsx
+++ src/FormContainer.jsx
@@ -1,11 +1,89 @@
-export default function FormContainer({ label, icon, children = [] }) {
+import InputElem from "./InputElem";
+import Form from "./Form";
+import generalIcon from "./assets/account.svg";
+import educationIcon from "./assets/school.svg";
+import experienceIcon from "./assets/professional.svg";
+
+export default function FormContainer({
+  generalInfoStates,
+  setGeneralInfoStates,
+}) {
+  const largeInput = 300;
+  const smallInput = 120;
+
+  const doubleSmInput = {
+    display: "flex",
+    justifyContent: "space-between",
+  };
   return (
-    <form action="" className="general-info-form">
-      <div className="form-header">
-        <img src={icon} alt={label + " form icon"} className="form-icon" />
-        <h2 className="form-title">{label}</h2>
-      </div>
-      {children.map((child) => child)}
-    </form>
+    <div className="forms-container">
+      <Form label="General Information" icon={generalIcon}>
+        <InputElem
+          label="First Name"
+          inputWidth={largeInput}
+          value={generalInfoStates.firstName}
+          onChange={setGeneralInfoStates}
+        />
+        <InputElem
+          label="Last Name"
+          inputWidth={largeInput}
+          value={generalInfoStates.lastName}
+          onChange={setGeneralInfoStates}
+        />
+        <InputElem
+          type="email"
+          label="Email"
+          inputWidth={largeInput}
+          value={generalInfoStates.email}
+          onChange={setGeneralInfoStates}
+        />
+        <InputElem
+          type="tel"
+          label="Phone Number"
+          inputWidth={largeInput}
+          value={generalInfoStates.phoneNumber}
+          onChange={setGeneralInfoStates}
+        />
+        <InputElem
+          label="Location"
+          inputWidth={largeInput}
+          value={generalInfoStates.location}
+          onChange={setGeneralInfoStates}
+        />
+        <InputElem
+          type="textarea"
+          label="Summary"
+          inputWidth={largeInput}
+          value={generalInfoStates.summary}
+          onChange={setGeneralInfoStates}
+        />
+      </Form>
+
+      <Form label="Education" icon={educationIcon}>
+        <InputElem label="School" inputWidth={largeInput} />
+        <InputElem label="Field of Study" inputWidth={largeInput} />
+        <div style={doubleSmInput}>
+          <InputElem label="Start Date" inputWidth={smallInput} />
+          <InputElem label="End Date" inputWidth={smallInput} />
+        </div>
+        <div className="form-buttons-container">
+          <button className="btn-clear">Clear Inputs</button>
+          <button className="btn-add">Add</button>
+        </div>
+      </Form>
+
+      <Form label="Experience" icon={experienceIcon}>
+        <InputElem label="Prueba" inputWidth={largeInput} />
+        <InputElem label="Field of Study" inputWidth={largeInput} />
+        <div style={doubleSmInput}>
+          <InputElem label="Start Date" inputWidth={smallInput} />
+          <InputElem label="End Date" inputWidth={smallInput} />
+        </div>
+        <div className="form-buttons-container">
+          <button className="btn-clear">Clear Inputs</button>
+          <button className="btn-add">Add</button>
+        </div>
+      </Form>
+    </div>
   );
 }
